61475b223a087a870e460825fd14313a601b7efe
ronaldoss8 <ronaldosssantos1@gmail.coml>
2025-01-05 16:56:30 -0300

    Initial commit

---
+++ src/main/java/com/CRUD2/StoreProject1/StoreProject1Application.java
@@ -0,0 +1,13 @@
+package com.CRUD2.StoreProject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreProject1Application.class, args);
+	}
+
+}
